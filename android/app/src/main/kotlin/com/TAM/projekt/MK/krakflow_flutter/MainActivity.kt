fb6e0381a981c14b484f5057f5e7b39265848645
Maksymilian Kotula <maksymilian.kotula@gmail.com>
2026-03-23 21:56:43 +0100

    lab_03 krakflow home screen

---
+++ android/app/src/main/kotlin/com/TAM/projekt/MK/krakflow_flutter/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.TAM.projekt.MK.krakflow_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+void main() {
+  runApp(MyApp());
+}
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("KrakFlow"),
+        ),
+        body: Center(child:Column(children: [
+          Text("KrakoFlow"),
+          Text("Organizacja studiów"),
+          Text("Dzisiejsze zadaia"),
+
+        ],),),
+      ),
+    );
+  }
+}
